a96e2749799e46dbf6dff23caec1eddf34112682
Rigoberto martinez <donojaci@gmail.com>
2022-07-19 14:47:39 -0500

    aregando la funcion que nos permite ver las categorias en la app

--- src/main.js
+++ src/main.js
@@ -19,5 +19,25 @@ const getTrendingMoviePreview = async () => {
         content.appendChild(movieContainer)
     });
 } 
-getTrendingMoviePreview()
 
+const getCategoriesPreview = async () => {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`)
+    const data = await response.json()
+    const content = document.getElementById('categoriesPreview-list')
+    const categories = data.genres
+    const categoriesPreview = categories.forEach(category => {
+        const categoryContainer = document.createElement('div')
+        categoryContainer.classList.add('category-container')
+
+        const categoryTitle = document.createElement('h3')
+        categoryTitle.id = `id${category.id}`
+        categoryTitle.classList.add('category-title');
+        categoryTitle.innerHTML = `${category.name}`
+
+        categoryContainer.appendChild(categoryTitle)
+        content.appendChild(categoryContainer)
+    });
+}
+
+getCategoriesPreview()
+getTrendingMoviePreview()
